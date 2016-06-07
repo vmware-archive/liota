9a137e9b3e15be281701aab4ef446b29dc43ad23
Piyush Masrani <pmasrani@vmware.com>
2016-06-07 17:00:22 +0530

    Added example for sending data to vRops alone.

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
     data_files=[('/etc/liota/example', ['example/graphite_simulated.py',
                 'example/vrops_graphite_dk300_sample.py',
                 'example/vrops_graphite_DellEdge5K_sample.py',
+                'example/vrops_simulated.py',
                 'example/graphite_withTemp.py',
                 'example/graphite_event_based.py',
                 'example/sampleProp.conf']),
