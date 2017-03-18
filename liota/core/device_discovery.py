57c01f5fea7bbb13e2685c32752104b89a47b72b
winniex1 <winniex@vmware.com>
2017-03-18 11:31:25 -0700

    Add supports in the device discovery: 1. form a relationship between newly discovered device and gateway 2. update the organization group property for the newly discovered device

--- liota/core/device_discovery.py
+++ liota/core/device_discovery.py
@@ -100,6 +100,8 @@ class DiscoveryThread(Thread):
                     try:
                         # retrieve device info file storage directory
                         self.dev_file_path = config.get('IOTCC_PATH', 'dev_file_path')
+                        # retrieve organization info file storage directory
+                        self.org_file_path = config.get('IOTCC_PATH', 'iotcc_path')
                     except ConfigParser.ParsingError as err:
                         log.error('Could not parse log config file' + err)
                         exit(-4)
@@ -112,6 +114,8 @@ class DiscoveryThread(Thread):
                             else:
                                 log.error('Could not create device file storage directory')
                                 exit(-4)
+                    if not os.path.exists(self.org_file_path):
+                        log.error('No organization info file (iotcc.json)')
                     try:
                         # retrieve discovery cmd pip
                         self.cmd_messenger_pipe = os.path.abspath(
@@ -217,6 +221,8 @@ class DiscoveryThread(Thread):
         self._config['package_path'] = self.package_path
         # device info storage path
         self._config['dev_file_path']= self.dev_file_path
+        # organization (edgesystem+devices) info storage path
+        self._config['org_file_path']= self.org_file_path
 
     #-----------------------------------------------------------------------
     # This method is used to handle listing commands
@@ -446,10 +452,29 @@ class DiscoveryThread(Thread):
         try:
             with self.discovery_lock:
                 reg_dev = graphite.register(dev)
-                #graphite.set_properties(reg_dev, prop_dict)
+                graphite.set_properties(reg_dev, prop_dict)
         except:
             return None, None
         return dev, reg_dev
+
+    def add_organization_properties(self, iotcc, prop_dict):
+        import json
+
+        # Get organization info file path
+        iotcc_json_path = self._config['org_file_path']
+        if iotcc_json_path == '':
+            return prop_dict
+        try:
+            with open(iotcc_json_path, 'r') as f:
+                iotcc_details_json_obj = json.load(f)["iotcc"]
+            f.close()
+        except IOError, err:
+            return prop_dict
+
+        org_group_properties = iotcc_details_json_obj["OGProperties"]
+        # merge org properties into prop_dict
+        new_prop_dict = dict(prop_dict.items() + org_group_properties.items())
+        return new_prop_dict
 
     def reg_device_iotcc(self, name, dev_type, prop_dict):
         from liota.entities.devices.device import Device
@@ -467,6 +492,7 @@ class DiscoveryThread(Thread):
         iotcc = self.pkg_registry.get("iotcc")
 
         dev = Device(name, systemUUID().get_uuid(name), dev_type)
+        prop_dict = self.add_organization_properties(iotcc, prop_dict)
         # Register device
         try:
             with self.discovery_lock:
@@ -474,6 +500,13 @@ class DiscoveryThread(Thread):
                 iotcc.set_properties(reg_dev, prop_dict)
         except:
             return None, None
+
+        # TBM: temporarily assume devices will be attached to the only edge system
+        if self.pkg_registry.has("iotcc_edge_system"):
+            iotcc_edge_system = self.pkg_registry.get("iotcc_edge_system")
+            iotcc.create_relationship(iotcc_edge_system, reg_dev)
+        else:
+            log.warning("iotcc_edge_system package is not loaded, please load it!")
         return dev, reg_dev
 
     def device_msg_process(self, data):
