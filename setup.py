7e97e919a6bf3f55da6620cb7c4e503acf6cea0f
Vaibhav Kohli <vkohli@vmware.com>
2017-04-17 16:19:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open('README.md') as f:
 
 setup(
     name='liota',
-    version='0.2.1',
+    version='0.2.4',
     packages=find_packages(exclude=["*.json", "*.txt"]),
     description='IoT Agent',
     long_description=long_description,
