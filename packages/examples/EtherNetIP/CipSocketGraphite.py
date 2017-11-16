4ea7ba3a6e86194b513372afdfe5fb9466d89a72
NithyaElango <30429054+NithyaElango@users.noreply.github.com>
2017-11-16 16:28:12 +0530

    Update CipSocketGraphite.py

--- packages/examples/EtherNetIP/CipSocketGraphite.py
+++ packages/examples/EtherNetIP/CipSocketGraphite.py
@@ -36,7 +36,7 @@ from liota.dccs.graphite import Graphite
 from liota.entities.metrics.metric import Metric
 from liota.entities.edge_systems.dell5k_edge_system import Dell5KEdgeSystem
 from liota.lib.utilities.utility import read_user_config
-from liota.device_comms.EtherNetIP_DeviceComms import EtherNetIPDeviceComms
+from liota.device_comms.EtherNetIP_DeviceComms import CipEtherNetIpDeviceComms
 from liota.entities.devices.simulated_device import SimulatedDevice
 
 
@@ -59,7 +59,7 @@ class PackageClass(LiotaPackage):
 
         self.config = read_user_config(config_path + '/sampleProp.conf')
 
-        self.ethernetIP_conn =  EtherNetIPDeviceComms(host=self.config['EtherNetIP'], port, timeout, dialect,
+        self.ethernetIP_conn =  CipEtherNetIpDeviceComms(host=self.config['EtherNetIP'], port, timeout, dialect,
                                                 profiler, udp=False, broadcast=False, source_address)
 
         ethernet_device = SimulatedDevice(self.config['DeviceName'], "Test")
